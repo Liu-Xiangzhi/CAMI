0215b5184256c2fa39f95ce7f8ab1e9c152f7d64
Liu Xiangzhi <lxzbuaa@126.com>
2024-09-04 12:38:07 +0800

    refactor project structure

---
+++ camic/test/testcases/a.c
@@ -0,0 +1,36 @@
+/*#include <stdio.h>
+
+#ifndef __CAMIC__
+#error a
+#endif  
+__STDC__
+__STDC_EMBED_NOT_FOUND__ 
+__STDC_EMBED_FOUND__ 
+__STDC_EMBED_EMPTY__
+__STDC_HOSTED__
+__STDC_UTF_16__
+__STDC_UTF_32__
+__STDC_VERSION__
+__STDC_ISO_10646__
+__STDC_MB_MIGHT_NEQ_WC__
+__STDC_ANALYZABLE__
+__STDC_IEC_60559_BFP__
+__STDC_IEC_559__
+__STDC_IEC_60559_DFP__
+__STDC_IEC_60559_COMPLEX__
+__STDC_IEC_60559_TYPES__
+__STDC_IEC_559_COMPLEX__
+__STDC_LIB_EXT1__
+__STDC_NO_ATOMICS__
+__STDC_NO_COMPLEX__
+__STDC_NO_THREADS__
+__STDC_NO_VLA__
+int main()
+{
+    printf("hello world\n");
+}
+*/
+#pragma a b c
+constexpr int \u4f60 = 0;
+constexpr int b = 你; # ##
+a1 0x1'23'4ull 1.e-10f u'a' . "abc" u"\U00004f60" +=>>=+- / *<:%%bbc+d
